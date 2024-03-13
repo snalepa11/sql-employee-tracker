1a1dc19689692f1e5d71e72e5f0a335fefe5a07d
Snalepa <snalepa1998@gmail.com>
2024-03-13 14:17:09 -0500

    server.js

--- seeds.sql
+++ seeds.sql
@@ -1,11 +1,13 @@
 INSERT INTO departments (department)
-VALUES (Engineering),
+VALUES 
+       (Engineering),
        (Finance),
        (Legal),
        (Sales);
 
 INSERT INTO roles (title, department, salary)
-VALUES ('Sales Lead', 'Sales', 100000),
+VALUES 
+       ('Sales Lead', 'Sales', 100000),
        ('Sales Person', "Sales", 80000),
        ('Lead Engineer', 'Engineering', 150000),
        ('Software Engineer', 'Engineering', 120000),
@@ -15,7 +17,8 @@ VALUES ('Sales Lead', 'Sales', 100000),
        ('Lawyer', 'Legal', 190000)
 
 INSERT INTO employees (first_name, last_name, title, department, salary, manager)
-VALUES ('John','Doe','Sales Lead', 'Sales', 100000),
+VALUES 
+       ('John','Doe','Sales Lead', 'Sales', 100000),
        ('Mike', 'Chan','Sales Person', "Sales", 80000, 'John Doe'),
        ('Ashley','Rodriguez','Lead Engineer', 'Engineering', 150000),
        ('Kevin','Tupik','Software Engineer', 'Engineering', 120000, 'Ashley Rodriguez'),
@@ -24,6 +27,6 @@ VALUES ('John','Doe','Sales Lead', 'Sales', 100000),
        ('Sarah', 'Lourd', 'Legal Team Lead', 'Legal', 250000),
        ('Tom', 'Allen', 'Lawyer', 'Legal', 190000, 'Sarah Lourd')
 
-       SELECT businesses.business_id, businesses.business_name, locations.location_name, locations.location_manager 
+SELECT businesses.business_id, businesses.business_name, locations.location_name, locations.location_manager 
 FROM businesses
 JOIN locations ON businesses.location_id = locations.location_id;
